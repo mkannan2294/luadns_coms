588b6a7c0021768a573d0e022197a3a491eb11ce
Masrur Khan <64892327+mkannan2294@users.noreply.github.com>
2020-06-12 20:50:45 +0600

    Create mechatron2694.com.lua

---
+++ mechatron2694.com.lua
@@ -0,0 +1,15 @@
+-- File: mechatron2694.com.lua
+-- Zone: mechatron2694.com
+-- SOA record is automatically generated
+-- Variable _a is replaced with zone name
+-- _a = "mechatron2694.com"
+
+-- A records
+a(_a, "192.168.244.173")
+
+-- CNAME records
+cname("www", _a)
+
+-- CAA records
+caa("", "letsencrypt.org", "issue")
+caa("", "mailto: mkannan22@mechatron2694.com", "iodef")
